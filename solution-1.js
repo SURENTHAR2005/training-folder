6b9a6858a4f5a8a54136595f35341b80900a16ad
SURENTHAR2005 <surentharvb2005@gmail>
2025-06-19 14:32:22 +0530

    changes in solution

--- solution-1.js
+++ solution-1.js
@@ -1,9 +1,12 @@
+//typing
 console.log("i want to become a developer");
 
+//datatypes
 let a=3;
 console.log("3"+a);
 console.log("4"-a);
 
+//loop and operator
 let b=100;
 while(b>0){
     if(b==90){
@@ -20,3 +23,22 @@ while(b>0){
     }
 }
 
+//array
+numbers=["one","two","three","four","five","six","seven"];
+numbers.push("eight");
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+let num1=numbers.length;
+
+//loop
+for (let j=0;j<num1;j++){
+    console.log(numbers[j]);
+}
+
+//operator
+if(num1){
+    console.log("this is not empty list");
+}else{
+    console.log("this is empty list");
+}
