4153a50c203f737123dc614853ac659be83736cd
SURENTHAR2005 <surentharvb2005@gmail.com>
2025-06-20 09:55:38 +0530

    june 20

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -6,3 +6,7 @@ console.log("2"+2);
 console.log("4"-2);
 console.log("5"/2);
 console.log("10"*3);
+
+// template string
+let name="surenthar V B";
+console.log(`my name is ${name}`);
---
+++ javascript/jun_20_training/temp.js
@@ -0,0 +1,13 @@
+//25 degree to fahrenheit
+let deg=25;
+let fah=((deg)*(9/5))+32;
+console.log("Fahernhit : " + Math.round(fah));
+//-5 degree to fahrenheit
+let deg1=-5;
+let fah1=((deg1)*(9/5))+32;
+console.log("Fahernheit : " + Math.round(fah1));
+
+//fahrenheit to degree
+let fah2=86;
+let deg2=((fah2-32)*(5/9));
+console.log("Degree : "+Math.round(deg2));
